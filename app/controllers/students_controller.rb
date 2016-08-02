9ace8dcee24a27001924b9f36f876fda05eb983e
kishorecherukuror <kishorecheruku.ror@gmail.com>
2016-08-02 15:34:33 +0530

    first commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,19 @@
+class StudentsController < ApplicationController
+  def create
+    @name = "raju"
+    @age = 25
+    @address = "vijayawada"
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def delete
+  end
+end
